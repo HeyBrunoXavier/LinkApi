a972207ef9454476a5045950e52b5ce9995b3159
HeyBrunoXavier <brunoxavier5230@gmail.com>
2021-06-13 21:55:32 -0300

    routes

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const pedidoController = require("../contollers/Controller.js");
+const pedidoController = require("../controllers/Controller.js");
 
 router.get('/v1/orders/', pedidoController.getAll);
 router.post('/v1/orders/', pedidoController.insert);
